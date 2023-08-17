81b81a74ab36d461300f21d0a4d1ee66a3fdd772
babi7777 <hb.contrat@gmail.com>
2023-08-17 20:23:59 +0200

    update api

--- api.py
+++ api.py
@@ -6,23 +6,19 @@
 
 from flask import Flask, request, jsonify
 import pandas as pd
-import multiprocessing
-from zipfile import ZipFile
 import joblib
 import requests
 import io
 
 app = Flask(__name__)
 
-# Charger les données et le modèle en cache
 def load_data():
     data_url = "https://github.com/babi7777/scoring-model-credit-risk/raw/main/X_test.zip"
     response = requests.get(data_url)
     with io.BytesIO(response.content) as zip_file:
         with ZipFile(zip_file, "r") as z:
             data = pd.read_csv(z.open('X_test.csv'), index_col='SK_ID_CURR', encoding='utf-8')
-            available_ids = data.index.tolist()
-            return data, available_ids
+            return data
             
 def load_raw_data():    
     data_brut_url = "https://github.com/babi7777/scoring-model-credit-risk/raw/main/X_test_brut.zip"
@@ -37,40 +33,17 @@ def load_model():
     response = requests.get(model_url)
     model = joblib.load(io.BytesIO(response.content))
     return model
-def main():
-    process_data = multiprocessing.Process(target=load_data)
-    process_model = multiprocessing.Process(target=load_model)
 
-    process_data.start()
-    process_model.start()
-
-    process_data.join()
-    process_model.join()
-    
-data, available_ids = load_data()
+# Chargement initial des données et du modèle
+data = load_data()
 raw_data = load_raw_data()
 model = load_model()
 
-# Point API pour fournir la liste des ID clients
-@app.route('/api/clients', methods=['GET'])
-def get_clients():
-    return jsonify({"available_ids": available_ids})
-
-# Point API pour fournir les données d'un client (ID)
 @app.route('/api/client/<int:id>', methods=['GET'])
 def get_client_data(id):
-    if id in available_ids:
+    if id in raw_data.index:
         client_raw_data = raw_data.loc[id].to_dict()
         return jsonify(client_raw_data)
-    else:
-        return jsonify({"error": "Client ID not found"}), 404
-
-# Point API pour fournir les données prétraitées d'un client spécifique
-@app.route('/api/client_preprocessed/<int:id>', methods=['GET'])
-def get_client_preprocessed_data(id):
-    if id in available_ids:
-        client_data_preprocessed = data.loc[id].to_dict()
-        return jsonify(client_data_preprocessed)
     else:
         return jsonify({"error": "Client ID not found"}), 404
 
